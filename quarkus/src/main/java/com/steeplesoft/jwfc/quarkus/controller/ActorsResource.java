4622f291f6e1a234750a7ae223d895346efca008
Jason Lee <jason@steeplesoft.com>
2022-10-13 18:07:21 -0500

    Basic REST setup
    Fixup Maven deps
    Add FlywayMigrate bean to set up the DB
    Move testcontainer config for tests to a new profile
    Add annotations to entity

--- quarkus/src/main/java/com/steeplesoft/jwfc/quarkus/controller/ActorsResource.java
+++ quarkus/src/main/java/com/steeplesoft/jwfc/quarkus/controller/ActorsResource.java
@@ -1,19 +1,34 @@
 package com.steeplesoft.jwfc.quarkus.controller;
 
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.ws.rs.GET;
+import java.util.List;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.steeplesoft.jwfc.quarkus.model.Actor;
 import com.steeplesoft.jwfc.quarkus.repository.ActorRepository;
 import io.smallrye.mutiny.Uni;
 
-@RequestScoped
+@Path("actors")
+@ApplicationScoped
+@Produces("application/json")
+@Consumes("application/json")
 public class ActorsResource {
     @Inject
     ActorRepository repository;
 
     @GET
+    @Path("count")
     public Uni<Long> count() {
         return repository.count();
+    }
+
+    @GET
+    public Uni<List<Actor>> getActors() {
+        return repository.listAll();
     }
 }
--- quarkus/src/main/java/com/steeplesoft/jwfc/quarkus/model/Actor.java
+++ quarkus/src/main/java/com/steeplesoft/jwfc/quarkus/model/Actor.java
@@ -2,6 +2,7 @@ package com.steeplesoft.jwfc.quarkus.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -12,9 +13,13 @@ import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
 public class Actor extends PanacheEntityBase {
     @Id
     @GeneratedValue
+    @Column(name = "actor_id")
     public Long actorId;
+    @Column(name = "first_name")
     public String firstName;
+    @Column(name = "last_name")
     public String lastName;
+    @Column(name = "last_update")
     public LocalDateTime lastUpdate;
 
 }
