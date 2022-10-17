99874f2cdef98b11bab2ac2834d33dfafdc67f67
Jason Lee <jason@steeplesoft.com>
2022-10-17 14:54:17 -0500

    Customer support
    Add model, service, and repository

--- quarkus/src/main/java/com/steeplesoft/jwfc/quarkus/model/BaseModel.java
+++ quarkus/src/main/java/com/steeplesoft/jwfc/quarkus/model/BaseModel.java
@@ -3,15 +3,17 @@ package com.steeplesoft.jwfc.quarkus.model;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Version;
 
-public class BaseModel {
+public abstract class BaseModel {
 
     @Version
     @Column(name = "last_update")
-    public LocalDateTime lastUpdate = LocalDateTime.now();
+    public LocalDateTime lastUpdate;
 
+    @PrePersist
     @PreUpdate
     void updateTimestamp() {
         lastUpdate = LocalDateTime.now();
